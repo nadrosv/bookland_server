683306bc3996e62711ca91392751dabbdab53c55
Sylwester Nadrowski <nadrosv@gmail.com>
2016-11-24 22:13:45 +0100

    simple registration and token based authentication

--- src/main/java/bookland/Application.java
+++ src/main/java/bookland/Application.java
@@ -1,14 +1,38 @@
 package bookland;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import bookland.security.JwtFilter;
 
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
 @SpringBootApplication
 public class Application {
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/api/*");
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+		return registrationBean;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
--- src/main/java/bookland/controllers/MainController.java
+++ src/main/java/bookland/controllers/MainController.java
@@ -1,8 +1,14 @@
 package bookland.controllers;
 
+import java.util.Date;
+
+import javax.servlet.ServletException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,44 +16,66 @@ import org.springframework.web.bind.annotation.RestController;
 
 import bookland.dao.UserDao;
 import bookland.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RestController
+@RequestMapping("/")
 public class MainController {
 
 	@Autowired
 	private UserDao userDao;
 
-	@RequestMapping("/")
+	@Autowired
+	private PasswordEncoder pEncode;
+
+	@RequestMapping("")
 	@ResponseBody
 	public String index() {
 		return "Hello World?";
 	}
 
-	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	@ResponseBody
-	public Object fakeLogin(String login, String password) {
-		User user;
-		try {
-			user = userDao.findByUsername(login);
-			if (user == null) {
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-			} else if (user.getPassword().equals(password)) {
-//				String token = TokenService.createJWT("xxx", "Bookland", "req", 3600000);
-//				return new ResponseEntity<>(token, HttpStatus.OK);
-				 return new ResponseEntity<>(user, HttpStatus.OK);
-			} else {
-				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-			}
-		} catch (Exception ex) {
-			return new ResponseEntity<>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+	@SuppressWarnings("unused")
+	private static class UserLogin {
+		public String name;
+		public String password;
+		public String email;
+	}
+
+	@SuppressWarnings("unused")
+	private static class LoginResponse {
+		public String token;
+		public long userId;
+
+		public LoginResponse(final String token, final long userId) {
+			this.token = token;
+			this.userId = userId;
 		}
 	}
 
-//	@RequestMapping(path = "/tokentest", method = RequestMethod.POST)
-//	@ResponseBody
-//	public Object fakeAuth(String token) {
-//		String decode = TokenService.parseJWT(token);
-//		return decode;
-//	}
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public LoginResponse login(@RequestBody final UserLogin login) throws ServletException {
+		User user = userDao.findByUsername(login.name);
+		if (login.name == null || user == null) {
+			throw new ServletException("Invalid login");
+		}
+		if (!pEncode.matches(login.password, user.getPassword())) {
+			throw new ServletException("Invalid password");
+		}
+		return new LoginResponse(Jwts.builder().setSubject(login.name).claim("roles", user.getUsername())
+				.setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256, "secretkey").compact(), user.getId());
+	}
+
+	@RequestMapping(value = "register", method = RequestMethod.POST)
+	@ResponseBody
+	public Object fakeRegistration(@RequestBody final UserLogin reg) throws ServletException {
+		User user = userDao.findByUsername(reg.name);
+		if (user != null) {
+			throw new ServletException("User exists");
+		}
+		user = new User(reg.email, reg.name, pEncode.encode(reg.password));
+		userDao.save(user);
+		return new ResponseEntity<>(user.getId(), HttpStatus.OK);
+	}
 
 }
